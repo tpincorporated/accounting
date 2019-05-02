9862138e43a43c1ef1ab9aec78d5c36468210e74
unknown <gianalipao@gmail.com>
2019-05-02 15:33:12 +0000

    DEBIT MEMO ADD POSTING DETAILS and ACCOUNT DETAILS

--- application/accounts-receivable/ar-debit-memo.php
+++ application/accounts-receivable/ar-debit-memo.php
@@ -271,6 +271,83 @@
     </div>
 </div>
 
+<div class="modal fade" id="add-debit-memo-posting-details-modal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class='page-title'>
+                    New Posting Details
+                    <button class="close" data-dismiss="modal">&times;</button>
+                </div>
+            </div>
+            <div class="modal-body">
+                <form class="form-horizontal" method='post' id='addchartofaccountsmodal-form'  enctype='multipart/form-data'>
+                    <div class='col-md-4'>
+
+                    </div>
+                    <div class='col-md-offset-1 col-md-6'>
+                    	<div class="form-group">
+                            <label class='control-label'>Select Type</label>
+                            <select class='form-control add chartofaccountsmodal-type select2' name='addchartofaccountsmodal-type'>
+								<option value="Detail">Detail</option>
+								<option value="Detail">Header</option>
+                            </select>
+                        </div>
+                        <div class="form-group"> 
+
+                        </div>
+                    </div>
+                </form>
+                <br>
+            </div>
+            <div class="modal-footer">
+                <div class="text-center">
+                    <button class='btn btn-blue2 mybtn' id='addchartofaccountsmodal-uploadbtn'>Save</button>
+                    <button class='btn btn-blue2 mybtn modal-cancelbtn' >Cancel</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="add-debit-memo-account-details-modal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class='page-title'>
+                    New Account Details
+                    <button class="close" data-dismiss="modal">&times;</button>
+                </div>
+            </div>
+            <div class="modal-body">
+                <form class="form-horizontal" method='post' id='addchartofaccountsmodal-form'  enctype='multipart/form-data'>
+                    <div class='col-md-4'>
+
+                    </div>
+                    <div class='col-md-offset-1 col-md-6'>
+                    	<div class="form-group">
+                            <label class='control-label'>Select Type</label>
+                            <select class='form-control add chartofaccountsmodal-type select2' name='addchartofaccountsmodal-type'>
+								<option value="Detail">Detail</option>
+								<option value="Detail">Header</option>
+                            </select>
+                        </div>
+                        <div class="form-group"> 
+
+                        </div>
+                    </div>
+                </form>
+                <br>
+            </div>
+            <div class="modal-footer">
+                <div class="text-center">
+                    <button class='btn btn-blue2 mybtn' id='addchartofaccountsmodal-uploadbtn'>Save</button>
+                    <button class='btn btn-blue2 mybtn modal-cancelbtn' >Cancel</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <script type="text/javascript">
@@ -306,7 +383,7 @@
                         {display: 'Amount', name : 'debitamount', width : 200, sortable : true, align: 'right'}
                 ],
                 buttons : [
-                        {name: 'Add', bclass: 'add', onpress : ''},
+                        {name: 'Add', bclass: 'add', onpress : showadddebitmemopostingdetailsmodal},
                         {separator: true},
                         {name: 'Delete', bclass: 'delete', onpress : ''}
                         /*{separator: true},
@@ -341,7 +418,7 @@
                         {display: 'Amount', name : 'creditaccountnumber', width : 150, sortable : true, align: 'right'}
                 ],
                 buttons : [
-                        {name: 'Add', bclass: 'add', onpress : ''},
+                        {name: 'Add', bclass: 'add', onpress : showadddebitmemoaccountdetailsmodal},
                         {separator: true},
                         {name: 'Delete', bclass: 'delete', onpress : ''}
                         /*{separator: true},
@@ -402,7 +479,15 @@
        		});
 
 	       
+			   function showadddebitmemopostingdetailsmodal(){
+			$("#add-debit-memo-posting-details-modal").modal('show')
+		}
+
+		function showadddebitmemoaccountdetailsmodal(){
+			$("#add-debit-memo-account-details-modal").modal('show')
+		}
 			
+
     		userAccess();
 
 
